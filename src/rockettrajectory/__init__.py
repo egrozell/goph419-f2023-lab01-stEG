9a7544d6131a48efce574ce1bf84b87c69857ee0
egrozell <ethan.grozell@ucalgary.ca>
2023-09-13 17:06:08 -0700

    added def launch_angle_range implemented equation 17

---
+++ src/rockettrajectory/__init__.py
@@ -0,0 +1,9 @@
+import numpy as np
+def launch_angle_range(ve_v0, alpha, tol_alpha)
+    while 
+    phi = np.arcsin((1+alpha)*np.sqrt(1-(alpha/(1+alpha))*(ve_v0)**2)) 
+    return phi_range
+def main()
+    
+if __name__ == '__main__': 
+    main()
