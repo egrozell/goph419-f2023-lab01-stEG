025ee6bac10c0bbb6c8050ff433e19b6702e9807
egrozell <ethan.grozell@ucalgary.ca>
2023-09-13 17:57:27 -0700

    updated launch_angle_range

--- src/rockettrajectory/__init__.py
+++ src/rockettrajectory/__init__.py
@@ -1,9 +1,13 @@
 import numpy as np
-def launch_angle_range(ve_v0, alpha, tol_alpha)
-    while 
-    phi = np.arcsin((1+alpha)*np.sqrt(1-(alpha/(1+alpha))*(ve_v0)**2)) 
+def launch_angle_range(ve_v0, alpha, totl_alpha)
+    n = 1 
+    x = (1+alpha)*np.sqrt(1-(alpha/(1+alpha))*(ve_v0)**2) 
+    while x <= 1.0 && x >= 0
+        xsum = (2x)**(2n)/(n**2*((np.factorial()*2n))/((n*np.factorial()))**2)
+        x = np.sin(np.sqrt(.5*xsum))
+        n += 1
     return phi_range
 def main()
-    
+
 if __name__ == '__main__': 
     main()
