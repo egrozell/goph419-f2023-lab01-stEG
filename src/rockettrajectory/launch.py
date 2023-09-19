37b38612dfb37215d6a1f52f78446c60a27b9a5f
egrozell <ethan.grozell@ucalgary.ca>
2023-09-19 15:46:03 -0700

    added launch.py with arcsin implementation

---
+++ src/rockettrajectory/launch.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def arcsin(x):
+    """Computes the inverse sine of x on the range [-pi/2 to pi/2]
+
+    inputs
+    float phi
+
+    returns
+    float
+        a radian as a float.
+
+    """
+    for n in range (10):
+        n+=1
+        top=(2*x)**(2*n)
+        bot=(n**2)*(np.math.factorial(2*n)/((np.math.factorial(n))**2))
+        arcsin_sqrt=arcsin_sqrt+.5*top/bot
+    arcsin=np.sqrt(arcsin_sqrt)
+    return arcsin
+
+def launch_angle(ve_v0, alpha):
+    pass
