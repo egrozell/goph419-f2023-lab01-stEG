3e59c7049a51d7ca633498f4c8af298e261d200f
egrozell <ethan.grozell@ucalgary.ca>
2023-09-20 16:35:16 -0700

    made packages more oop friendly by moving arcsin to arcsin.py added launch_angle and launch_angle_range classes.

--- src/rockettrajectory/launch.py
+++ src/rockettrajectory/launch.py
@@ -1,23 +1,54 @@
 import numpy as np
-
-def arcsin(x):
-    """Computes the inverse sine of x on the range [-pi/2 to pi/2]
-
-    inputs
-    float phi
-
-    returns
-    float
-        a radian as a float.
-
-    """
-    for n in range (10):
-        n+=1
-        top=(2*x)**(2*n)
-        bot=(n**2)*(np.math.factorial(2*n)/((np.math.factorial(n))**2))
-        arcsin_sqrt=arcsin_sqrt+.5*top/bot
-    arcsin=np.sqrt(arcsin_sqrt)
-    return arcsin
+import arcsin as arc
 
 def launch_angle(ve_v0, alpha):
+    """launch angle from vertical given velocity ratio and target altitude ratio
+
+    inputs
+    float velocity ratio and alpha
+
+    returns
+    float phi in radians
+    """
+    alpha1= 1+alpha
+    sqrt = np.sqrt(1-(alpha/alpha1)*(ve_v0**2))
+    phi0 =arc.arcsin((alpha1)*sqrt)
+    return phi0
+
+def launch_angle_range(ve_v0, alpha, tol_alpha):
+    """Calculate the range of launch angles for a given velocity ration, target altitude ration, and tolerance
+    inputs
+    float ve_v0, alpha, tol_alpha
+
+    returns max_angle, min_angle
+    """
+    min_alpha = (1 - tol_alpha)*alpha
+    max_alpha = (1 + tol_alpha)*alpha
+    math_temp=1 - ((min_alpha/1+min_alpha)*vev0**2))
+    if math_tem>0:
+        sin_phi=(1+min_alpha)*np.sqrt(math_temp)
+        min_angle=arcsin(sin_phi)
+        if max_angle>np.pi()/2 or max_angle<0:
+            max_angle=none
+    else:
+        max_angle = none
+    math_temp=1 - ((max_alpha/1+max_alpha)*vev0**2))
+    if math_temp>0:
+        sin_phi=(1+max_alpha)*np.sqrt(math_temp)
+    return max_angle, min_angle
+
+def min_altitude_ratio(ve_v0):
+    pass
+
+def max_altitude_ratio(ve_v0):
+    pass
+
+def min_velocity_ratio(alpha):
+    """Computes minimum possible velocity ratio for a given target peak altitude.
+    """
+    pass
+
+def min_velocity_ratio(alpha):
+    """Computes minimum possible velocity ratio for a given target peak altitude.
+    """
     pass
